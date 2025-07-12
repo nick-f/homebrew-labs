08b8f47c4b94e6225e65cc78de5bd3b23a08b484
nick-f <nick-f@users.noreply.github.com>
2025-07-12 12:02:10 +0000

    t 1.2.1

--- Formula/t.rb
+++ Formula/t.rb
@@ -1,8 +1,8 @@
 class T < Formula
   desc "Search project directories and open a new Tmux session for the selected project"
   homepage "https://github.com/nick-f/t"
-  url "https://github.com/nick-f/t/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "dc9a0e13e345e710db52d3dd1a5fc0901519ea09d9f9fb6e0943e0df153ae3ca"
+  url "https://github.com/nick-f/t/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "a5d2cf6e6f4d28cb3abeef8dccb03fb76aa8f8cf4d0d7d7646c2fb9bcb072356"
   license "MIT"
 
   depends_on "bash"
